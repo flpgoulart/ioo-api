a0d7f9825383a819a3cd59bcb81fb01f30a9bea2
Felipe Goulart <flpgoulart@gmail.com>
2019-01-08 12:40:50 +0000

    adding city model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808085137) do
+ActiveRecord::Schema.define(version: 20190108123848) do
+
+  create_table "cities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "cep_begin"
+    t.integer  "cep_end"
+    t.string   "uf"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                                default: "",      null: false
