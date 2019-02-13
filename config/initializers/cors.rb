e26e4e490b123d3b87b36f3453160c2ae0ceee6e
Felipe Goulart <flpgoulart@gmail.com>
2019-02-13 16:02:38 -0200

    add exception on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :delete, :options]
   end
 end
